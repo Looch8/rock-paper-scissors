ed7c0af42a8bf3ba14622ded1e0dee9ea399d8b5
Luke Wheldale <lukewheldale@gmail.com>
2022-06-08 20:32:46 +0930

    wrote some pseudocode to plan out project

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+//computerPlay
+// create function that randomly returns either 'Rock, Paper, or scissors'
+
+//playerSelection
+// create a function 'playerSelection' that will prompt user to choose either 'rock, paper or scissors'
+//  playerSelection paramaters should be case-insensitive - user can input rock, ROCK, RocK etc 
+
+//results
+// return results of this function call
+// NEW function called 'game ()' that keeps score
+// Declare winners.
+
+//refer to odin website for more detailed instructions.
+
 console.log("hello")

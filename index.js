6458c327a8d6e00c20cfa684162fe89808008ec0
Luke Wheldale <lukewheldale@gmail.com>
2022-06-20 19:21:25 +0930

    added function that generates a random value

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 //computerPlay
 // create function that randomly returns either 'Rock, Paper, or scissors'
+function computerPlay() {
+    randomValue = Math.floor(Math.random() * 3);
+    if (randomValue == "0") {
+        return "Rock";
+    }
+    else if (randomValue == "1") {
+        return "Paper";
+    } 
+    else {
+        return "Scissors";
+    }
+}
+
+console.log(computerPlay());
+
 
 //playerSelection
 // create a function 'playerSelection' that will prompt user to choose either 'rock, paper or scissors'
@@ -11,5 +26,3 @@
 // Declare winners.
 
 //refer to odin website for more detailed instructions.
-
-console.log("hello")

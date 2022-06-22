5d5cb0f8ac928d0e50dfb706c1288134a3d8fb9a
Luke Wheldale <lukewheldale@gmail.com>
2022-06-22 15:15:38 +0930

    need to fix game function to keep track of score and declare a winner

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+let playerScore = 0
+let computerScore = 0
+
 //computerPlay
 // create function that randomly returns either 'Rock, Paper, or scissors'
 function computerPlay() {
@@ -13,33 +16,36 @@ function computerPlay() {
     }
 }
 
-
 //play single round of RPS
 
 function playRound(playerSelection, computerSelection) {
-    //convert player selection to all lowercase
     
-
     //rock
     if (playerSelection === "rock" && computerSelection === "scissors") {
         return "You Win! Rock beats Scissors";
+        playerScore++;
     }
     else if (playerSelection === "rock" && computerSelection === "paper") {
         return "You Lose! Paper beats Rock!";
+        computerScore++;
     }
     //paper
     if (playerSelection === "paper" && computerSelection === "rock") {
         return "You Win! Paper beats Rock";
+        playerScore++;
     }
     else if (playerSelection === "paper" && computerSelection === "scissors") {
         return "You Lose! Scissors beats Paper";
+       computerScore++;
     }
     //scissors    
     if (playerSelection === "scissors" && computerSelection === "paper") {
         return "You Win! Scissors beats Paper";
+        playerScore++;
     }
     else if (playerSelection === "scissors" && computerSelection === "rock") {
         return "You Lose! Rock beats Scissors";
+        computerScore++;
     }
     //draw
     else if (playerSelection === computerSelection) {
@@ -49,19 +55,41 @@ function playRound(playerSelection, computerSelection) {
     else {
         return "Please play the game properly";
     }
-}
-//Maybe turn this into an array ^??
 
+    if (playerScore === 5) {
+        return "Player wins!";
+    }
+    if (computerScore === 5) {
+        return "Computer wins!";
+    }
+}
+
+//convert player selection to all lowercase
 const playerSelection = prompt("Rock, Paper, or Scissors?").toLowerCase();
 const computerSelection = computerPlay();
 
 console.log(playRound(playerSelection, computerSelection));
-// create a function 'playerSelection' that will prompt user to choose either 'rock, paper or scissors'
-//  playerSelection paramaters should be case-insensitive - user can input rock, ROCK, RocK etc 
+ 
+/*
+ Write a NEW function called game(). Call the playRound function inside of 
+ this one to play a 5 round game that keeps score and reports a winner or 
+ loser at the end.
+ */
+//function game() {
+    //for (let i = 0; i < 5; i++) {
+        //text += "Round " + i;
+    //}
 
-//results
-// return results of this function call
-// NEW function called 'game ()' that keeps score
-// Declare winners.
+    //if (computerScore === 5 || playerScore === 5) {
+        //return "Game over!"
+    //}
+    
+
+//}
+
+ 
+ 
+
+ // Declare winners.
 
 //refer to odin website for more detailed instructions.

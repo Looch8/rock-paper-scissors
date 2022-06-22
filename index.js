1728f90373a304542995c0c015b170657868e3be
Luke Wheldale <lukewheldale@gmail.com>
2022-06-22 13:51:52 +0930

    added all options for playRound

--- index.js
+++ index.js
@@ -18,15 +18,36 @@ function computerPlay() {
 
 function playRound(playerSelection, computerSelection) {
     //convert player selection to all lowercase
+    
 
+    //rock
     if (playerSelection === "rock" && computerSelection === "scissors") {
         return "You Win! Rock beats Scissors";
     }
     else if (playerSelection === "rock" && computerSelection === "paper") {
         return "You Lose! Paper beats Rock!";
     }
-    else if (playerSelection === "rock" && computerSelection === "rock") {
-        return "Draw!"
+    //paper
+    if (playerSelection === "paper" && computerSelection === "rock") {
+        return "You Win! Paper beats Rock";
+    }
+    else if (playerSelection === "paper" && computerSelection === "scissors") {
+        return "You Lose! Scissors beats Paper";
+    }
+    //scissors    
+    if (playerSelection === "scissors" && computerSelection === "paper") {
+        return "You Win! Scissors beats Paper";
+    }
+    else if (playerSelection === "scissors" && computerSelection === "rock") {
+        return "You Lose! Rock beats Scissors";
+    }
+    //draw
+    else if (playerSelection === computerSelection) {
+        return "Draw!";
+    }
+    //Non-viable entry
+    else {
+        return "Please play the game properly";
     }
 }
 //Maybe turn this into an array ^??

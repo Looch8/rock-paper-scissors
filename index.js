32ecf720063a359534385c7bd665798394a2292b
Luke Wheldale <lukewheldale@gmail.com>
2022-06-22 10:07:10 +0930

    created a function that plays a single round of RPS, so far have only added the selection for Rock. Will need to make it case insensitive and add paper and scissor options too

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //computerPlay
 // create function that randomly returns either 'Rock, Paper, or scissors'
 function computerPlay() {
-    randomValue = Math.floor(Math.random() * 3);
+     const randomValue = Math.floor(Math.random() * 3);
     if (randomValue == "0") {
         return "Rock";
     }
@@ -13,10 +13,24 @@ function computerPlay() {
     }
 }
 
-console.log(computerPlay());
 
+//play single round of RPS
 
-//playerSelection
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === "Rock" && computerSelection === "Scissors") {
+        return "You Win! Rock beats Scissors";
+    }
+    else if (playerSelection === "Rock" && computerSelection === "Paper") {
+        return "You Lose! Paper beats Rock!";
+    }
+    else if (playerSelection === "Rock" && computerSelection === "Rock") {
+        return "Draw!"
+    }
+}
+
+const playerSelection = "Rock";
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
 // create a function 'playerSelection' that will prompt user to choose either 'rock, paper or scissors'
 //  playerSelection paramaters should be case-insensitive - user can input rock, ROCK, RocK etc 
 

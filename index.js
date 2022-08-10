2c8945eacef7e8670ff9e7be784e9f070166f942
Luke Wheldale <lukewheldale@gmail.com>
2022-08-10 16:03:17 +0930

    need to work out how to update player score

--- index.js
+++ index.js
@@ -1,95 +1,62 @@
+const computerSelectionDisplay = document.getElementById('computer-selection')
+const playerSelectionDisplay = document.getElementById('player-selection')
+const resultDisplay = document.getElementById('result')
+const playerScoreDisplay = document.getElementById('player-score')
+const computerScoreDisplay = document.getElementById('computer-score')
+const possibleChoices = document.querySelectorAll('button')
+let playerSelection
+let computerSelection
+let result
 let playerScore = 0
 let computerScore = 0
 
-//computerPlay
-// create function that randomly returns either 'Rock, Paper, or scissors'
-function computerPlay() {
-     const randomValue = Math.floor(Math.random() * 3);
-    if (randomValue == "0") {
-        return "rock";
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
+    playerSelection = e.target.id
+    playerSelectionDisplay.innerHTML = playerSelection
+    generateComputerSelection()
+    getResult()
+
+}))
+
+function generateComputerSelection() {
+    const randomNumber = Math.floor(Math.random() * 3) + 1
+    console.log(randomNumber)
+
+    if (randomNumber === 1) {
+        computerSelection = 'rock'
     }
-    else if (randomValue == "1") {
-        return "paper";
-    } 
-    else {
-        return "scissors";
+    if (randomNumber === 2) {
+        computerSelection = 'paper'
     }
+    if (randomNumber === 3) {
+        computerSelection = 'scissors'
+    }
+    computerSelectionDisplay.innerHTML = computerSelection
 }
 
-//play single round of RPS
-
-function playRound(playerSelection, computerSelection) {
-    
-    //rock
-    if (playerSelection === "rock" && computerSelection === "scissors") {
-        return "You Win! Rock beats Scissors";
-        playerScore++;
+function getResult() {
+    if (computerSelection === playerSelection) {
+        result = 'Draw!'
     }
-    else if (playerSelection === "rock" && computerSelection === "paper") {
-        return "You Lose! Paper beats Rock!";
-        computerScore++;
+    if (
+       (computerSelection === 'rock' && playerSelection === 'scissors') ||
+       (computerSelection === 'paper' && playerSelection === 'rock')  ||
+       (computerSelection === 'scissors' && playerSelection === 'paper')
+     ) {
+        result = 'you lost!'
+        computerScore++
     }
-    //paper
-    if (playerSelection === "paper" && computerSelection === "rock") {
-        return "You Win! Paper beats Rock";
-        playerScore++;
-    }
-    else if (playerSelection === "paper" && computerSelection === "scissors") {
-        return "You Lose! Scissors beats Paper";
-       computerScore++;
-    }
-    //scissors    
-    if (playerSelection === "scissors" && computerSelection === "paper") {
-        return "You Win! Scissors beats Paper";
-        playerScore++;
-    }
-    else if (playerSelection === "scissors" && computerSelection === "rock") {
-        return "You Lose! Rock beats Scissors";
-        computerScore++;
-    }
-    //draw
-    else if (playerSelection === computerSelection) {
-        return "Draw!";
-    }
-    //Non-viable entry
-    else {
-        return "Please play the game properly";
+    if ( 
+       (playerSelection === 'rock' && computerSelection === 'scissors') ||
+       (playerSelection === 'paper' && computerSelection === 'rock')  ||
+       (playerSelection === 'scissors' && computerSelection === 'paper')
+     ) {
+        result = 'you won!'
+        playerScore++  
     }
 
-    if (playerScore === 5) {
-        return "Player wins!";
-    }
-    if (computerScore === 5) {
-        return "Computer wins!";
-    }
+    resultDisplay.innerHTML = result
+    playerScoreDisplay.innterHTML = playerScore
+    computerScoreDisplay.innerHTML = computerScore 
 }
 
-//convert player selection to all lowercase
-const playerSelection = prompt("Rock, Paper, or Scissors?").toLowerCase();
-const computerSelection = computerPlay();
-
-console.log(playRound(playerSelection, computerSelection));
- 
-/*
- Write a NEW function called game(). Call the playRound function inside of 
- this one to play a 5 round game that keeps score and reports a winner or 
- loser at the end.
- */
-//function game() {
-    //for (let i = 0; i < 5; i++) {
-        //text += "Round " + i;
-    //}
-
-    //if (computerScore === 5 || playerScore === 5) {
-        //return "Game over!"
-    //}
-    
-
-//}
-
- 
- 
-
- // Declare winners.
-
-//refer to odin website for more detailed instructions.
